cf15dcd41b134eedd58077c9181ca3ae18e6cbd5
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2025-09-21 22:31:37 +0200

    vUpdate

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 200,
-    height: 600,
+    width: 600,
+    height: 200,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true
